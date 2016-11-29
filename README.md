84ae91ccfba65b25076afd701d3c805cbf17d838
Peng.Li <nowind_lee@qq.com>
2016-11-29 23:36:54 +0800

    servlet demo

---
+++ README.md
@@ -0,0 +1,8 @@
+Servlet Demo
+============
+
+```
+gradle jettyRun
+```
+
+Then visit <http://localhost:8080/gradle-jetty-servlet-demo>
---
+++ src/main/java/example/HelloWorldServlet.java
@@ -0,0 +1,20 @@
+package example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorldServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<body>");
+        out.println("<h1>Hello World!</h1>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
